daccef5012308608ae621498d7779e5bb3f16f74
eric jacobs <eric@ebj.dev>
2026-06-20 17:03:18 -0600

    lane lane-a: disable experimental warnings once, globally (no restore); unwrap 5 callers

--- lib/space_architect/warnings.rb
+++ lib/space_architect/warnings.rb
@@ -4,14 +4,10 @@ module SpaceArchitect
   module Warnings
     module_function
 
-    def without_experimental
-      return yield unless Warning.respond_to?(:[]) && Warning.respond_to?(:[]=)
+    def disable_experimental!
+      return unless Warning.respond_to?(:[]) && Warning.respond_to?(:[]=)
 
-      original = Warning[:experimental]
       Warning[:experimental] = false
-      yield
-    ensure
-      Warning[:experimental] = original unless original.nil?
     end
   end
 end
